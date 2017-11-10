6017be01807ad4041361366794cab74c0001d899
Mateus Marques <marques.m05@gmail.com>
2017-11-10 21:36:23 -0200

    create network directory

---
+++ list-github-repository/Network/PullRequestService.swift
@@ -0,0 +1,9 @@
+//
+//  PullRequestService.swift
+//  list-github-repository
+//
+//  Created by Mateus Marques on 10/11/17.
+//  Copyright © 2017 Mateus Marques. All rights reserved.
+//
+
+import Foundation
---
+++ list-github-repository/Network/RepositoryService.swift
@@ -0,0 +1,9 @@
+//
+//  RepositoryService.swift
+//  list-github-repository
+//
+//  Created by Mateus Marques on 10/11/17.
+//  Copyright © 2017 Mateus Marques. All rights reserved.
+//
+
+import Foundation
