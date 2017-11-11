cd2cb32de8e7946fcbb8fc5c57559d06bc2aeaff
Mateus Marques <marques.m05@gmail.com>
2017-11-10 22:02:13 -0200

    crete class pullRequestService and implement method makeRequest

--- list-github-repository/Network/PullRequestService.swift
+++ list-github-repository/Network/PullRequestService.swift
@@ -6,4 +6,43 @@
 //  Copyright © 2017 Mateus Marques. All rights reserved.
 //
 
-import Foundation
+import CoreData
+import Alamofire
+
+struct PullRequestService {
+    
+    static func makeRequest(
+        forRepository repository: RepositoryEntity,
+        context: NSManagedObjectContext,
+        completion: @escaping (_ error: AppError?) -> Void)
+    {
+        
+        let projectName = repository.fullName!
+        
+        Alamofire.request(Route.pullRequests(projectName: projectName).get, method: .get)
+            .validate()
+            .responseJSON { (dataResponse) in
+            
+                guard dataResponse.result.isSuccess, let data = dataResponse.data else {
+                    let error = dataResponse.error?.localizedDescription ?? ""
+                    completion(PullRequestError.serviceResponse(localizedError: error))
+                    return
+                }
+                
+                do {
+                    let jsonPullRequests = try JSONDecoder().decode([JSONPullRequest].self, from: data)
+                    
+                    PullRequestDAO.save(jsonPullRequests: jsonPullRequests, repositoryId: repository.id, inContext: context) { error in
+                        if let error = error {
+                            completion(PullRequestError.save(localizedError: error.localizedDescription))
+                        } else {
+                            completion(nil)
+                        }
+                    }
+                } catch {
+                    print("Malformed data received from service")
+                    completion(PullRequestError.save(localizedError: error.localizedDescription))
+                }
+        }
+    }
+}
