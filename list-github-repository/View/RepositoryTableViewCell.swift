16295e1ae1c10ce1b20d54bbbb797a4b5c57d77e
Mateus Marques <marques.m05@gmail.com>
2017-11-12 21:03:21 -0200

    create layout for repository screen

---
+++ list-github-repository/View/RepositoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RepositoryTableViewCell.swift
+//  list-github-repository
+//
+//  Created by Mateus Marques on 12/11/17.
+//  Copyright © 2017 Mateus Marques. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
